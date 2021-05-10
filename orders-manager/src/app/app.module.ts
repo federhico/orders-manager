8e1772174c34f015dc48f571b1ab1c9954195999
AgusGelos <juanagustingelos1@gmail.com>
2021-05-10 09:07:30 -0300

    Deleted Shared & Core Components & created Module

--- orders-manager/src/app/app.module.ts
+++ orders-manager/src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreComponent } from './core/core.component';
-import { SharedComponent } from './shared/shared.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoreComponent,
-    SharedComponent
   ],
   imports: [
     BrowserModule,
