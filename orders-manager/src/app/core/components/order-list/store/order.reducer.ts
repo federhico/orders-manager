eedcf0e4752caa129af78a22094707e6490be796
AgusGelos <juanagustingelos1@gmail.com>
2021-06-18 12:02:10 -0300

    Before implementing Effects

--- orders-manager/src/app/core/components/order-list/store/order.reducer.ts
+++ orders-manager/src/app/core/components/order-list/store/order.reducer.ts
@@ -4,44 +4,48 @@ import { Orders } from 'src/app/core/models/Orders';
 import * as OrdersAction from './order.actions';
 
 
-export const initialState: Orders[] = [     {
-  _id: 'INICIAL',
-  title: 'my beautifull order',
-  description: 'Something about my order',
-  status: 'On Hold',
-  sender: {
-      id: 100,
-      name: 'Juan Arias'
-  },
-  destinationAddress: 'Saavedra 68',
-  destinationCity: 'Villa Carlos Paz',
-  destinationCountry: 'Argentina',
-  destinationCoordinates: {
-      lat: -123456,
-      long: -54321
-  },
-  price: 100,
-  taxApplied: 0,
-  weight: 100,
-  messureUnit: 'KG',
-  createdOn: '2021-05-06:00:000:00',
-  favourite: true
-} ];
+// export const initialState: Orders[] = [     {
+//   _id: 'INICIAL',
+//   title: 'my beautifull order',
+//   description: 'Something about my order',
+//   status: 'On Hold',
+//   sender: {
+//       id: 100,
+//       name: 'Juan Arias'
+//   },
+//   destinationAddress: 'Saavedra 68',
+//   destinationCity: 'Villa Carlos Paz',
+//   destinationCountry: 'Argentina',
+//   destinationCoordinates: {
+//       lat: -123456,
+//       long: -54321
+//   },
+//   price: 100,
+//   taxApplied: 0,
+//   weight: 100,
+//   messureUnit: 'KG',
+//   createdOn: '2021-05-06:00:000:00',
+//   favourite: true
+// } ];
 export const ordersFeatureKey = 'State';
+export const ordersInitialState: OrdersState = {
+  orders: [],
+  error: null
+};
 export interface OrdersState {
-  orders: Orders[];
+  orders: Orders[]
+  error: any;
 }
 
 // tslint:disable-next-line: variable-name
 export const _orderReducer = createReducer(
-  initialState,
+  // initialState,
+  ordersInitialState,
   // tslint:disable-next-line: no-shadowed-variable
   on(OrdersAction.loadOrders, state => state),
   on(
     OrdersAction.loadOrdersSuccess,
     (state, { orderList }) => {
-      console.log(state);
-      console.log(orderList);
       return {
 
         ...state,
